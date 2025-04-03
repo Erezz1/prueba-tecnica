df1c4208a2b287d06478aeca80d9610cc3ce906b
Neto Pérez <netoperez@Netos-MacBook-Pro.local>
2025-04-03 17:36:29 -0600

    Se crea rama build

--- src/constants/index.ts
+++
@@ -1,4 +0,0 @@
-export const API_URL = import.meta.env.VITE_API_URL as string;
-export const API_KEY = import.meta.env.VITE_API_KEY as string;
-
-export const DATA_SET_VALUE = "product";
--- src/context/CartContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-import { CartStore } from "../interfaces/types";
-
-export const CartContext = createContext<CartStore | null>(null);
